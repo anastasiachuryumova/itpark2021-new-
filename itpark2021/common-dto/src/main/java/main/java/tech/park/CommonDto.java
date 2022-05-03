23e95de9ebec880997df5a135a2146bd9deaf540
anastasiachuryumova <93443434+anastasiachuryumova@users.noreply.github.com>
2022-05-03 18:46:09 +0300

    Add files via upload

---
+++ itpark2021/common-dto/src/main/java/main/java/tech/park/CommonDto.java
@@ -0,0 +1,13 @@
+package main.java.tech.park;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonDto {
+    private Integer id;
+    private String name;
+}
