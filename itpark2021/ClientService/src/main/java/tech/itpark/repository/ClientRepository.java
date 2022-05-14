57a1eee5ef69c530192a1462d7d76cf29f08a05a
anastasiachuryumova <93443434+anastasiachuryumova@users.noreply.github.com>
2022-05-14 17:39:37 +0300

    Delete itpark2021/ClientService directory

--- itpark2021/ClientService/src/main/java/tech/itpark/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.itpark.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.itpark.domain.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
